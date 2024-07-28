d181bd05eb3468b0246258d221e3c5cef65a768e
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-28 14:31:03 +0300

    setting the navbar in the layout

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <section className="w-full text-center flex-col">
+    <section className="w-full flex-center text-center flex-col">
       <h1 className="head_text text-center">
         Descover & Share
         <br />
