47d35721074b953dbde56e8b7c888546a65fa88f
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-28 09:37:24 +0300

    implementing the layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,21 @@
 import "@styles/globals.css";
 
-const layout = () => {
-  return <div>layout</div>;
+export const metadata = {
+  title: "Promptopia",
+  description: "Descover & Share AI Prompts",
 };
 
-export default layout;
+const RootLayout = () => {
+  return (
+    <html>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
