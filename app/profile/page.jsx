aa1886ab6e7952dfe36c4f4b106c484b2bd3c2c4
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-31 14:27:48 +0300

    editing and deleting implementation complete user can edit and delete a prompt.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,6 +9,7 @@ import Profile from "@components/Profile";
 const MyProfile = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -20,8 +21,27 @@ const MyProfile = () => {
     if (session?.user.id) fetchPost();
   }, []);
 
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        // displaying post without the deleted posts
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <div>
